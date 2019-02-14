2abde5d3837471c32e1f72718ff5f7f50a13a8eb
cdanielgg <cdanielgg@hotmail.com>
2019-02-14 02:14:57 +0100

    probando las fotos

--- app/src/main/java/com/example/carlos/secondapp/libro/LibroInsertarActivity.java
+++ app/src/main/java/com/example/carlos/secondapp/libro/LibroInsertarActivity.java
@@ -1,12 +1,19 @@
 package com.example.carlos.secondapp.libro;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.carlos.secondapp.R;
 import com.example.carlos.secondapp.constantes.G;
@@ -14,6 +21,14 @@ import com.example.carlos.secondapp.pojos.Libro;
 import com.example.carlos.secondapp.proveedor.LibroProveedor;
 
 public class LibroInsertarActivity extends AppCompatActivity {
+
+    EditText editTextLibroTitulo;
+    EditText editTextLibroPaginas;
+    ImageView imageViewLibro;
+    Bitmap foto = null;
+
+    final int PETICION_CAPTURAR_IMAGEN = 1;
+    final int PETICION_ESCOGER_IMAGEN_DE_GALERIA = 2;
 
 
     @Override
@@ -23,9 +38,64 @@ public class LibroInsertarActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_detalle_activity);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);// se usa para mostrar la flecha para volver al padre de la activity
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        editTextLibroTitulo = (EditText) findViewById(R.id.editTextLibroTitulo);
+        editTextLibroPaginas = (EditText) findViewById(R.id.editTextLibroPaginas);
 
+        imageViewLibro = (ImageView) findViewById(R.id.image_view_libro);
+
+        final ImageButton imageButtonCamara = (ImageButton) findViewById(R.id.buttonCamara);
+        imageButtonCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sacarFoto();
+            }
+        });
+
+        final ImageButton imageButtonImagenDeGaleria = (ImageButton) findViewById(R.id.buttonImagenDeGaleria);
+        imageButtonImagenDeGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                elegirFotoDeGaleria();
+            }
+        });
+    }
+
+    void sacarFoto(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent, PETICION_CAPTURAR_IMAGEN);
+        }
+    }
+
+    void elegirFotoDeGaleria(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, PETICION_ESCOGER_IMAGEN_DE_GALERIA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case PETICION_CAPTURAR_IMAGEN:
+                if(resultCode == RESULT_OK){
+                    foto = (Bitmap) data.getExtras().get("data");
+                    imageViewLibro.setImageBitmap(foto);
+                } else {
+                    //El usuario canceló la toma de la foto
+                }
+                break;
+            case PETICION_ESCOGER_IMAGEN_DE_GALERIA:
+                if(resultCode == RESULT_OK){
+                    imageViewLibro.setImageURI(data.getData());
+                    foto = ((BitmapDrawable) imageViewLibro.getDrawable()).getBitmap();
+                } else {
+                    //El usuario canceló la toma de la foto
+                }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -83,8 +153,8 @@ public class LibroInsertarActivity extends AppCompatActivity {
 
         }
         //introduce los datos en el proveedor de contenidos
-        Libro libro = new Libro(G.SIN_VALOR_INT, titulo, paginas);
-        LibroProveedor.insertRecord(getContentResolver(),libro);
+        Libro libro = new Libro(G.SIN_VALOR_INT, titulo, paginas, foto);
+        LibroProveedor.insertRecord(getContentResolver(),libro, this);
         // lo finaliza
         finish();
 
